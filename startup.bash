95b116fced1acc2aec220e93afa54cee780df2f9
Tom Xiong <txion001@ucr.edu>
2017-06-07 17:41:57 -0700

    new

--- startup.bash
+++ startup.bash
@@ -4,4 +4,5 @@ echo "Starting Roscore"
 source /home/docker/ws/x_os/devel/setup.bash
 ROS_PACKAGE_PATH=/home/docker/ws/x_os/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks
 ROS_MASTER_URI=http://127.0.0.1:8001
+sleep 5s
 roslaunch -p 8001 x_main x_os.launch

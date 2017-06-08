0bf6309e1612421585af8a01952bc8b63db1a1df
Tom Xiong <txion001@ucr.edu>
2017-06-07 17:57:15 -0700

    new

--- startup.bash
+++ startup.bash
@@ -2,5 +2,7 @@
 python -m SimpleHTTPServer 8000 &
 echo "Starting Roscore"
 source /home/docker/ws/x_os/devel/setup.bash
+ROS_PACKAGE_PATH=/home/docker/ws/x_os/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks
+ROS_MASTER_URI=http://127.0.0.1:11311
 sleep 5s
-roslaunch x_main x_os.launch
+roslaunch -p 8001 x_main x_os.launch
